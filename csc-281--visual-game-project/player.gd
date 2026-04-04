1d8c69f1152a0e24c9c026540658cd1f9a79e41d
xavierkaratz-coder <xavierkaratz@gmail.com>
2026-04-04 16:58:21 -0400

    More movement changes
    Did these in class on Wednesday

--- csc-281--visual-game-project/player.gd
+++ csc-281--visual-game-project/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-@onready var player = get_parent().get_node("player")
+var speed = 200
 
+<<<<<<< Updated upstream
 var speed = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,18 +11,19 @@ func _ready() -> void:
 #test
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+=======
+func _physics_process(delta: float) -> void:
+>>>>>>> Stashed changes
 	var move = Vector2.ZERO
-	
-	if (Input.is_action_pressed("ui_right") ):
-		move += Vector2(+1,0)
-		
-	if (Input.is_action_pressed("ui_left") ):
-		move += Vector2(-1,0)
-		
-	if (Input.is_action_pressed("ui_up") ):
-		move += Vector2(0,-1)
-		
-	if (Input.is_action_pressed("ui_down") ):
-		move += Vector2(0,+1)
-	
-	player.position += move.normalized()*speed
+
+	if Input.is_action_pressed("ui_right"):
+		move += Vector2(1, 0)
+	if Input.is_action_pressed("ui_left"):
+		move += Vector2(-1, 0)
+	if Input.is_action_pressed("ui_up"):
+		move += Vector2(0, -1)
+	if Input.is_action_pressed("ui_down"):
+		move += Vector2(0, 1)
+
+	velocity = move.normalized() * speed
+	move_and_slide()
